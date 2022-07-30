a080f92247fd9d012bbb6ea6fdbcd92d7e626c99
Jonas <jonas-focke@mailbox.org>
2022-07-30 12:03:07 +0200

    feat: 🚧 solution rust day 1 part 2

--- 2021 - Rust/day_2/solution.rs
+++ 2021 - Rust/day_2/solution.rs
@@ -19,21 +19,14 @@ fn part1() -> i32 {
                 }
             }
         }
-
-        // print!("line: {:?}\n", input_vec[i]);
     }
     
     //from here on, input_vec is a Vector with the input
 
-    let mut solution: i32 = 0;
-
     let mut forward: i32 = 0;
     let mut vertical: i32 = 0;
 
-    for (i, instruction) in input_vec.iter().enumerate() {
-
-        solution = forward + vertical;
-
+    for (_i, instruction) in input_vec.iter().enumerate() {
         if instruction.0 == String::from("forward") {
             forward += instruction.1;
         } else if instruction.0 == String::from("up") {
@@ -41,39 +34,51 @@ fn part1() -> i32 {
         } else if instruction.0 == String::from("down") {
             vertical += instruction.1
         }
-        // print!("instruction: {:?}, forward: {}, vertical: {}\n", instruction, forward, vertical);
     }
 
-    solution = forward * vertical;
-
-    solution    
+    forward * vertical
 }
 
-fn part2() {
-    // let filename = "input.txt";
-    // let file = File::open(filename).unwrap();
-    // let reader = BufReader::new(file);
-    // let mut input_vec: Vec<i32> = vec![];
-    // let mut solution: i32 = 0;
-   
-    // for line in reader.lines() {
-    //     let line = line.unwrap().parse().unwrap();
-    //     input_vec.push(line);
-    // }
+fn part2() -> i32 {
+    let filename = "input.txt";
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+    let mut input_vec: Vec<(String, i32)> = vec![];
 
-    // //from here on, input_vec is a Vector with the input
+    for (i, line) in reader.lines().enumerate() {
+        let line = line.unwrap();
+        input_vec.push((String::new(), 0));
+        for character in line.chars() {
+            if character != ' ' {
+                if character.is_numeric() == true {
+                    input_vec[i].1 = character as i32 - 0x30;
+                } else {
+                    input_vec[i].0.push(character);
+                }
+            }
+        }
+    }
     
-    // for (_i, _reading) in input_vec.iter().enumerate() {
-        
-    // }
-    // solution    
+    //from here on, input_vec is a Vector with the input
+
+    let mut forward: i32 = 0;
+    let mut depth: i32 = 0;
+    let mut aim: i32 = 0;
+
+    for (_i, instruction) in input_vec.iter().enumerate() {
+        if instruction.0 == String::from("forward") {
+            forward += instruction.1;
+            depth += instruction.1 * aim;
+        } else if instruction.0 == String::from("up") {
+            aim -= instruction.1
+        } else if instruction.0 == String::from("down") {
+            aim += instruction.1
+        }
+    }
+    forward * depth
 }
 
 fn main() {
-
     print!("Part 1: {}\n", part1());
     print!("Part 2: {:?}\n", part2());
-    print!("u32 max {}\n", u32::MAX);
-    print!("i32 max {}\n", i32::MAX);
-    print!("u16 max {}\n", u16::MAX);
 }
