3a872947b8a28fad20699b906506d79b2bc3649e
Jonas <jonas-focke@mailbox.org>
2022-08-11 21:34:57 +0200

    feat: 🚧 solution rust day 9 part 2

--- 2021 - Rust/day_9/solution.rs
+++ 2021 - Rust/day_9/solution.rs
@@ -22,12 +22,93 @@ fn part1() -> u16 {
 
 
 
-fn part2() -> i32 {
+fn part2() -> u32 {
     let dataset = read_dataset(false);
     
-    let return_value: i32 = -1;
+    let mut dataset_info_if_checked: Vec<Vec<(u8, bool)>> = vec!();
+    let mut temp_dataset_info_if_checked: Vec<Vec<(u8, bool)>>;
+    let mut basins: Vec<u16> = vec!();
 
-    return_value
+
+    for (i, row) in dataset.iter().enumerate() {
+        dataset_info_if_checked.push(vec!());
+        for (j, _point) in row.iter().enumerate() {
+            dataset_info_if_checked[i].push((dataset[i as usize][j as usize], false));
+        }
+    }
+
+    //logic to build the basins
+    temp_dataset_info_if_checked = dataset_info_if_checked.to_vec();
+    for index in 0..9 {
+        for (i, row) in dataset_info_if_checked.iter().enumerate() {
+            for (j, column) in row.iter().enumerate() {
+                if column.0 == index && column.1 == false {
+                    let ret_val = check_if_basin((i as u16, j as u16), temp_dataset_info_if_checked);
+                    basins.push(ret_val.0);
+                    temp_dataset_info_if_checked = ret_val.1;
+                }
+            }
+        }
+        dataset_info_if_checked = temp_dataset_info_if_checked.to_vec();
+    }
+
+    //find greatest three in dataset
+    let mut greatest_1: u16 = u16::MIN;
+    let mut greatest_2: u16 = u16::MIN;
+    let mut greatest_3: u16 = u16::MIN;
+
+    for basin in basins.iter() {
+        if basin > &greatest_1 {
+            greatest_2 = greatest_1;
+            greatest_3 = greatest_2;
+            greatest_1 = *basin;
+        }
+        if basin > &greatest_2 && basin != &greatest_1 {
+            greatest_3 = greatest_2;
+            greatest_2 = *basin;
+        }
+        if basin > &greatest_3 && basin != &greatest_1 && basin != &greatest_2 {
+            greatest_3 = *basin;
+        }
+    }
+
+    greatest_1 as u32 * greatest_2 as u32 * greatest_3 as u32
+}
+
+fn check_if_basin(point: (u16, u16), mut input: Vec<Vec<(u8, bool)>>) -> (u16, Vec<Vec<(u8, bool)>>) {
+    let mut return_value: u16 = 1;
+
+    input[point.0 as usize][point.1 as usize].1 = true;
+
+    //left
+    if point.1 > 0 && !input[point.0 as usize][point.1 as usize - 1].1 && input[point.0 as usize][point.1 as usize - 1].0 != 9 {
+        let ret_val = check_if_basin((point.0 as u16, point.1 - 1 as u16), input.to_vec());
+        return_value += ret_val.0;
+        input = ret_val.1;
+    }
+
+    //right
+    if point.1 < RANGE_X && !input[point.0 as usize][point.1 as usize + 1].1 && input[point.0 as usize][point.1 as usize + 1].0 != 9 {
+        let ret_val = check_if_basin((point.0, point.1 + 1), input.to_vec());
+        return_value += ret_val.0;
+        input = ret_val.1;
+    }
+
+    //up
+    if point.0 > 0 && !input[point.0 as usize - 1][point.1 as usize ].1 && input[point.0 as usize - 1][point.1 as usize].0 != 9 {
+        let ret_val = check_if_basin((point.0 as u16 - 1, point.1 as u16), input.to_vec());
+        return_value += ret_val.0;
+        input = ret_val.1;
+    }
+
+    //down
+    if point.0 < RANGE_Y - 1 && !input[point.0 as usize + 1][point.1 as usize].1 && input[point.0 as usize + 1][point.1 as usize].0 != 9 {
+        let ret_val = check_if_basin((point.0 + 1 as u16, point.1 as u16), input.to_vec());
+        return_value += ret_val.0;
+        input = ret_val.1;
+    }
+
+    (return_value, input)
 }
 
 fn check_if_low_point(point: (u16, u16), input: &Vec<Vec<u8>>) -> u16 {
@@ -105,9 +186,9 @@ fn main() {
     
     let solution_part_2 = part2();
     if PRODUCTION {
-        assert_eq!(solution_part_2, -1);
+        assert_eq!(solution_part_2, 920448);
     } else {
-        assert_eq!(solution_part_2, -1);
+        assert_eq!(solution_part_2, 1134);
     }
     print!("Part 2: {}\n", solution_part_2);
 }
